111d57e65cea9a0f5d57481bc274d651a451195f
JasonBenfrin <bhonemyatmin123@gmail.com>
2022-02-12 18:06:17 +0630

    process.env.PORT

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+require('dotenv').config()
 
 app.use(express.static(__dirname))
 
@@ -8,8 +9,9 @@ app.all("/",(req,res)=>{
 } );
 
 function keepAlive(){
-  app.listen(3000,()=>{
-    console.log("Alive")
+  const port = process.env.PORT || 3000
+  app.listen(port,()=>{
+    console.log(`Server listening at port ${port}`)
   })
 }
 

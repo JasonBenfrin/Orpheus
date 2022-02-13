a072aeb3a2fbe4ceacdacf06c20f4ba86c7bac6a
JasonBenfrin <bhonemyatmin123@gmail.com>
2022-02-13 16:29:27 +0630

    Fixed my stupid bug

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 3000
+const path = require('path')
 
 app.use(express.static(path.join(__dirname)));
 
